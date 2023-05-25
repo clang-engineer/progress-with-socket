5c0a6b2370604559909ca579b7f4c841ee53c05d
yorez <yorez@naver.com>
2023-05-25 15:24:08 +0900

    initial commit

---
+++ src/main/java/com/example/progresswithsocket/ProgressWithSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.progresswithsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressWithSocketApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProgressWithSocketApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/progresswithsocket/ProgressWithSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progresswithsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressWithSocketApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
